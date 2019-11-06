973c3b4f546f90c749562f7791db443a0a68d877
Vladislav Zubritsky <vzubritsky@tula.co>
2019-11-06 15:37:01 +0300

    initial

---
+++ RateLimiter.Tests/TimeoutLimitingStrategyTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace RateLimiter.Tests
+{
+	public class TimeoutLimitingStrategyTest
+	{
+		[Fact]
+		public void TryPass_ShouldPassOnFirstCall()
+		{
+			var timeOffSet = TimeSpan.MaxValue;
+			var target = new TimeoutLimitingStrategy(timeOffSet, () => DateTimeOffset.UtcNow);
+
+			var result = target.TryPass("42");
+
+			result.Should().Be(true);
+		}
+
+		[Fact]
+		public void TryPass_ShouldPassOnSecondDelayedCall()
+		{
+			var token = "42";
+
+			var dt1 = DateTimeOffset.UtcNow;
+			var timeOffSet = TimeSpan.FromSeconds(1);
+			var target = new TimeoutLimitingStrategy(timeOffSet, GetCurrentTime);
+			target.TryPass(token).Should().BeTrue();
+
+			dt1 = dt1.AddSeconds(2);
+
+			target.TryPass(token).Should().BeTrue();
+
+			DateTimeOffset GetCurrentTime() => dt1;
+		}
+
+		[Fact]
+		public void TryPass_ShouldNotPassOnSecondFastCall()
+		{
+			var token = "42";
+
+			var dt1 = DateTimeOffset.UtcNow;
+			var timeOffSet = TimeSpan.FromSeconds(2);
+			var target = new TimeoutLimitingStrategy(timeOffSet, GetCurrentTime);
+			target.TryPass(token).Should().BeTrue();
+
+			dt1 = dt1.AddSeconds(1);
+
+			target.TryPass(token).Should().BeFalse();
+
+			DateTimeOffset GetCurrentTime() => dt1;
+		}
+	}
+}
---
+++ RateLimiter/RateLimiter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RateLimiter
+{
+	public interface IRateLimiterStrategy
+	{
+		bool TryPass(string token);
+	}
+
+	public class LimitedPerTimeSpanStrategy : IRateLimiterStrategy
+	{
+		public bool TryPass(string token)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public class TimeoutLimitingStrategy : IRateLimiterStrategy
+	{
+		private readonly TimeSpan _timeOffset;
+		private readonly Func<DateTimeOffset> _getCurrentTime;
+		private Dictionary<string, DateTimeOffset> _history = new Dictionary<string, DateTimeOffset>();
+		private Object _lock = new Object();
+
+		public TimeoutLimitingStrategy(TimeSpan timeOffset, Func<DateTimeOffset> getCurrentTime)
+		{
+			_timeOffset = timeOffset;
+			_getCurrentTime = getCurrentTime ?? throw new ArgumentNullException(nameof(getCurrentTime));
+		}
+
+		public bool TryPass(string token)
+		{
+			bool success = true;
+			var currentCallTime = _getCurrentTime();
+
+
+			lock (_lock)
+			{
+				if (_history.TryGetValue(token, out var lastCallTime))
+				{
+					if (lastCallTime.Add(_timeOffset) > currentCallTime)
+						success = false;
+				}
+
+				_history[token] = currentCallTime;
+			}
+
+			return success;
+		}
+	}
+
+
+	public class RateLimiter
+	{
+		private readonly IRateLimiterStrategy _strategy;
+
+		public RateLimiter(IRateLimiterStrategy strategy)
+		{
+			_strategy = strategy ?? throw new ArgumentNullException(nameof(strategy));
+		}
+
+		public bool TryPass(string token) => _strategy.TryPass(token);
+	}
+}
