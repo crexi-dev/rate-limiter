e3004c80aff61623c0adf16cd0535afc9b67d3e7
Vladislav Zubritsky <vzubritsky@tula.co>
2019-11-28 18:15:27 +0300

    initial

--- RateLimiter.Tests/RateLimiterTest.cs
+++ RateLimiter.Tests/RateLimiterTest.cs
@@ -1,14 +1,54 @@
-﻿using NUnit.Framework;
+﻿using System;
+using Moq;
+using NUnit.Framework;
 
 namespace RateLimiter.Tests
 {
     [TestFixture]
     public class RateLimiterTest
     {
-        [Test]
-        public void Example()
+	    private Mock<IStorage> storage;
+	    private IRateLimiter target;
+	    private ResourceRequestLimit rule;
+
+	    private const string ResourceId = "qwe";
+	    private const string ClientId = "asd";
+
+		[OneTimeSetUp]
+	    public void SetUp()
+	    {
+			storage = new Mock<IStorage>();
+			rule = new ResourceRequestLimit(TimeSpan.FromSeconds(5));
+			target = new RateLimiter(storage.Object, rule);
+	    }
+
+	    [TestCase(1)]
+	    [TestCase(2)]
+	    [TestCase(3)]
+	    [TestCase(4)]
+        public void Denies_When_Limit_Exceeded(int secondsSinceLastCall)
         {
-            Assert.IsTrue(true);
-        }
-    }
+	        storage
+		        .Setup(x => x.GetLastUsage(ResourceId, ClientId))
+		        .Returns(DateTime.UtcNow - TimeSpan.FromSeconds(secondsSinceLastCall));
+
+	        var actual = target.IsRequestAllowed(ResourceId, ClientId);
+
+            Assert.IsFalse(actual);
+		}
+
+		[TestCase(14)]
+		[TestCase(24)]
+		[TestCase(6)]
+		public void Allow_When_Limit_Does_Not_Reached(int secondsSinceLastCall)
+		{
+			storage
+				.Setup(x => x.GetLastUsage(ResourceId, ClientId))
+				.Returns(DateTime.UtcNow - TimeSpan.FromSeconds(secondsSinceLastCall));
+
+			var actual = target.IsRequestAllowed(ResourceId, ClientId);
+
+			Assert.IsTrue(actual);
+		}
+	}
 }
---
+++ RateLimiter/RateLimiter.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RateLimiter
+{
+	public interface IRateLimiter
+	{
+		bool IsRequestAllowed(string resourceId, string clientId);
+	}
+
+	public class RateLimiter : IRateLimiter
+	{
+		private readonly IStorage storage;
+		private readonly IResourceRequestLimit limitRules;
+
+		public RateLimiter(IStorage storage, IResourceRequestLimit limitRules)
+		{
+			this.storage = storage;
+			this.limitRules = limitRules;
+		}
+
+		public bool IsRequestAllowed(string resourceId, string clientId)
+		{
+			var now = DateTime.UtcNow;
+			var lastUsage = storage.GetLastUsage(resourceId, clientId);
+			storage.SetLastUsage(resourceId, clientId, now);
+
+			if (lastUsage == null)
+			{
+				return true;
+			}
+
+			if (now - lastUsage.Value > limitRules.SinceLastCall)
+			{
+				return true;
+			}
+
+			return false;
+		}
+	}
+
+	public interface IStorage
+	{
+		DateTime? GetLastUsage(string resourceId, string clientId);
+		void SetLastUsage(string resourceId, string clientId, DateTime lastUsage);
+	}
+
+	public class ResourceRestrictionStorage : IStorage
+	{
+		private readonly Dictionary<string, Dictionary<string, DateTime>> resourceLastRequested = new Dictionary<string, Dictionary<string, DateTime>>();
+
+		public DateTime? GetLastUsage(string resourceId, string clientId)
+		{
+			if (!resourceLastRequested.ContainsKey(resourceId))
+			{
+				return null;
+			}
+
+			if (!resourceLastRequested[resourceId].ContainsKey(clientId))
+			{
+				return null;
+			}
+
+			return resourceLastRequested[resourceId][clientId];
+		}
+
+		public void SetLastUsage(string resourceId, string clientId, DateTime lastUsage)
+		{
+			if (!resourceLastRequested.ContainsKey(resourceId))
+			{
+				resourceLastRequested[resourceId] = new Dictionary<string, DateTime>();
+			}
+
+			resourceLastRequested[resourceId][clientId] = lastUsage;
+		}
+	}
+
+	public interface IResourceRequestLimit
+	{
+		TimeSpan SinceLastCall { get; }
+	}
+
+	public class ResourceRequestLimit : IResourceRequestLimit
+	{
+		public TimeSpan SinceLastCall { get; }
+
+		public ResourceRequestLimit(TimeSpan lastCallRestriction)
+		{
+			SinceLastCall = lastCallRestriction;
+		}
+	}
+
+
+
+	public interface IRule
+	{
+		bool IsRuleAccepted(string resourceId, string clientId, DateTime currentRequest);
+	}
+
+	public class SinceLastCallRule : IRule
+	{
+		private readonly IStorage storage;
+
+		public SinceLastCallRule(IStorage storage)
+		{
+			this.storage = storage;
+		}
+
+		public bool IsRuleAccepted(string resourceId, string clientId, DateTime currentRequest)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
