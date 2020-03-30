5fb55fed0a269887cd7befaca5152cc6e96a057e
joyoon <johnyoon20@gmail.com>
2020-03-30 07:02:55 -0500

    update test

--- RateLimiter.Tests/RateLimiter/RateLimiterTest.cs
+++ RateLimiter.Tests/RateLimiter/RateLimiterTest.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using RateLimiter.Implementation;
+using RateLimiter.Client;
 using RateLimiter.Repository;
 using RateLimiter.RulesEngine;
 using NSubstitute;
@@ -13,17 +13,47 @@ namespace RateLimiter.Tests
         [Test]
         public void Verify_True_WithinRatelimit()
         {
+            // arrange 
             var clientToken = "abc123";
+            var serverIP = "183.28.89.21";
             var requestDate = new DateTime(2020, 1, 1, 0, 0, 0, 500);   // 1/1/2020 12:00:05AM
             var lastUpdateDate = new DateTime(2020, 1, 1, 0, 0, 0, 0);   // 1/1/2020 12:00:00AM
-            var rule = new RegionRule(1, "US rule", RateLimitType.RequestsPerTimespan, RateLimitLevel.Default);
+
             var fakeRulesEngineClient = Substitute.For<IRulesEngineClient>();
             var fakeClientRepository = Substitute.For<IClientRepository>();
 
-            var rateLimiter = new RateLimiter(fakeClientRepository, fakeRulesEngineClient);
-            var result = rateLimiter.Verify(clientToken, requestDate, "183.28.89.21");
+            var rateLimiterProxy = Substitute.For<IRateLimiterProxy>();
+            rateLimiterProxy.Verify(clientToken, requestDate, serverIP).Returns(true);
+            var rateLimiterClient = new RateLimiterClient(rateLimiterProxy);
 
-            Assert.IsTrue(true);
+            // act
+            var result = rateLimiterClient.Verify(clientToken, requestDate, serverIP);
+
+            // assert
+            Assert.AreEqual(result, true);
+        }
+
+        [Test]
+        public void Verify_False_ExceedRatelimit()
+        {
+            // arrange 
+            var clientToken = "abc123";
+            var serverIP = "183.28.89.21";
+            var requestDate = new DateTime(2020, 1, 1, 0, 0, 0, 500);   // 1/1/2020 12:00:05AM
+            var lastUpdateDate = new DateTime(2020, 1, 1, 0, 0, 0, 0);   // 1/1/2020 12:00:00AM
+
+            var fakeRulesEngineClient = Substitute.For<IRulesEngineClient>();
+            var fakeClientRepository = Substitute.For<IClientRepository>();
+
+            var rateLimiterProxy = Substitute.For<IRateLimiterProxy>();
+            rateLimiterProxy.Verify(clientToken, requestDate, serverIP).Returns(false);
+            var rateLimiterClient = new RateLimiterClient(rateLimiterProxy);
+
+            // act
+            var result = rateLimiterClient.Verify(clientToken, requestDate, serverIP);
+
+            // assert
+            Assert.AreEqual(result, false);
         }
     }
 }
