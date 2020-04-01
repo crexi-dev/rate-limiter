f38c6225b8669ccd3ac502311ce659a7a0fc5369
AlexK <akuznetsov@softengine.com>
2020-04-01 11:56:23 -0700

    Added: mock Controller and APIs

---
+++ RateLimiter/Controller/Controller.cs
@@ -0,0 +1,36 @@
+﻿using RateLimiter.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RateLimiter.Controller
+{
+    internal class Controller
+    {
+        private readonly IRateLimiter _rate_limiter_service;
+
+        public Controller(IRateLimiter rate_limiter_service)
+        {
+            _rate_limiter_service = rate_limiter_service;
+        }
+
+        public int ApiFunction1(IClientRequest request)
+        {
+            if (!_rate_limiter_service.Validate("ApiFunction1", request))
+                return -1;
+
+            return 100;
+        }
+
+        public int ApiFunction2(IClientRequest request)
+        {
+            if (!_rate_limiter_service.Validate("ApiFunction2", request))
+                return -1;
+
+            return 200;
+        }
+
+    }
+}
