03500522a86dae06f321087bdbf74699046c0b88
HolyMeekrob <holy.meekrob@gmail.com>
2022-02-26 14:40:50 -0600

    Add combination utilities
    This will be a useful tool for consumbers of this library to be able
    to build custom classes from existing selectors and limiters.

---
+++ RateLimiter/TokenLimiters/Combination.cs
@@ -0,0 +1,33 @@
+﻿namespace RateLimiter.TokenLimiters
+{
+	public static class Combination
+	{
+		public static ITokenLimiter And(ITokenLimiter a, ITokenLimiter b) =>
+			new TokenLimiterBuilder(accessToken => a.IsRateLimited(accessToken) && b.IsRateLimited(accessToken));
+
+		public static ITokenLimiter Or(ITokenLimiter a, ITokenLimiter b) =>
+			new TokenLimiterBuilder(accessToken => a.IsRateLimited(accessToken) || b.IsRateLimited(accessToken));
+
+		public static ITokenLimiter Not(ITokenLimiter tokenLimiter) =>
+			new TokenLimiterBuilder(accessToken => !tokenLimiter.IsRateLimited(accessToken));
+
+
+		#region TokenLimiterBuilder
+
+		private delegate bool LimitChecker(IAccessToken accessToken);
+
+		private class TokenLimiterBuilder : ITokenLimiter
+		{
+			private readonly LimitChecker _limitChecker;
+
+			public TokenLimiterBuilder(LimitChecker limitChecker)
+			{
+				_limitChecker = limitChecker;
+			}
+
+			public bool IsRateLimited(IAccessToken accessToken) => _limitChecker.Invoke(accessToken);
+		}
+
+		#endregion TokenLimiterBuilder
+	}
+}
