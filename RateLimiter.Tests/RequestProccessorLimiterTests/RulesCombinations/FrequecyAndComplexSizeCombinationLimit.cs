d2f26d35f7fd75f443a3f240f12782dbe787040f
Lipotam-vostroh <Lipotam@gmail.com>
2022-09-19 22:00:03 +0300

    Add complex Limiter and a test for several limiters usage

---
+++ RateLimiter.Tests/RequestProccessorLimiterTests/RulesCombinations/FrequecyAndComplexSizeCombinationLimit.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using RateLimiter.Rules;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RateLimiter.Tests.RequestProccessorLimiterTests.RulesCombinations
+{
+    /// <summary>
+    /// A demo to use two rules. Request frequncy and odd-even balance control
+    /// </summary>
+    public class FrequecyAndComplexSizeCombinationLimit
+    {
+        private ILimiter testObject;
+
+        [SetUp]
+        public void TestInit()
+        {
+            testObject = new RequestProccessorLimiter(new RequestProcessor(), new List<ILimiteRule>());
+            testObject.AddRule(new Rules.RequestFrequencyLimiteRule(50));
+            testObject.AddRule(new OddOrEvenSizeByPeriodLimiteRule(1000, 1000));
+        }
+
+        [Test, Timeout(60*1000)]
+        public void Run100WithSize50_With20Success()
+        {
+            List<Task<Response>> tasks = new List<Task<Response>>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                tasks.Add(testObject.DoRequestAsync(new Request() { Size = 500 + i }));
+                Thread.Sleep(i * 5);
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            var successCount = tasks.Select(t => t.Result).Where(r => r.IsSuccessful).Count();
+            var failedCount = tasks.Select(t => t.Result).Where(r => !r.IsSuccessful).Count();
+
+            Assert.AreEqual(35, successCount); // Checked in Excel
+        }
+    }
+}
---
+++ RateLimiter/Rules/OddOrEvenSizeByPeriodLimiteRule.cs
@@ -0,0 +1,24 @@
+﻿namespace RateLimiter.Rules
+{
+    /// <summary>
+    /// The class is made for demo of using rules combination
+    /// </summary>
+    public class OddOrEvenSizeByPeriodLimiteRule : ILimiteRule
+    {
+        private RequestTotalSizePeriodLimiteRule oddNumbersRule;
+        private RequestTotalSizePeriodLimiteRule evenNumbersRule;
+
+        public OddOrEvenSizeByPeriodLimiteRule(long _periodMs, long _sizeLimit)
+        {
+            oddNumbersRule = new RequestTotalSizePeriodLimiteRule(_periodMs, _sizeLimit);
+            evenNumbersRule = new RequestTotalSizePeriodLimiteRule(_periodMs, _sizeLimit);
+        }
+
+        public bool CanPassNow(Request request)
+        {
+            return request.Size % 2 == 0 
+                ? oddNumbersRule.CanPassNow(request)
+                : evenNumbersRule.CanPassNow(request);
+        }
+    }
+}
