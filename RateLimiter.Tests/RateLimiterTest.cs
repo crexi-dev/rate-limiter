a6997a7a9067c63280345f1f7950aa48f68a363e
stdint <stdint@gmail.com>
2023-04-11 16:01:59 +0400

    Add RateLimiter and tests

--- RateLimiter.Tests/RateLimiterTest.cs
+++ RateLimiter.Tests/RateLimiterTest.cs
@@ -1,13 +1,96 @@
 ﻿using NUnit.Framework;
+using System;
 
 namespace RateLimiter.Tests;
 
 [TestFixture]
 public class RateLimiterTest
 {
-	[Test]
-	public void Example()
-	{
-		Assert.That(true, Is.True);
-	}
+    [Test]
+    public void TenRequestsPerTenSeconds()
+    {
+        string token = Guid.NewGuid().ToString();
+        var rateLimiter = new RateLimiter(t => new RateLimiterQueue()
+            .EnableRateLimit(10, new TimeSpan(0, 0, 10)));
+        Assert.True(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 0, 0)));
+        Assert.True(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 0, 1)));
+        Assert.True(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 0, 2)));
+        Assert.True(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 0, 3)));
+        Assert.True(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 0, 4)));
+        Assert.True(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 0, 5)));
+        Assert.True(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 0, 6)));
+        Assert.True(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 0, 7)));
+        Assert.True(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 0, 8)));
+        Assert.True(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 0, 9)));
+        Assert.False(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 0, 10)));
+        Assert.True(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 10, 1)));
+        Assert.True(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 10, 2)));
+    }
+
+    [Test]
+    public void OneSecondPeriod()
+    {
+        string token = Guid.NewGuid().ToString();
+        var rateLimiter = new RateLimiter(t => new RateLimiterQueue()
+            .EnablePeriodLimit(new TimeSpan(0, 0, 1)));
+        Assert.True(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 0, 0)));
+        Assert.False(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 0, 1)));
+        Assert.False(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 0, 2)));
+        Assert.True(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 1, 3)));
+        Assert.False(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 1, 4)));
+    }
+
+    [Test]
+    public void TenRequestPerTenSecondsWithOneSecondPeriod()
+    {
+        string token = Guid.NewGuid().ToString();
+        var rateLimiter = new RateLimiter(t => new RateLimiterQueue()
+            .EnableRateLimit(5, new TimeSpan(0, 0, 10))
+            .EnablePeriodLimit(new TimeSpan(0, 0, 1)));
+        Assert.True(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 0, 0)));
+        Assert.False(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 0, 1)));
+        Assert.False(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 0, 2)));
+        Assert.True(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 1, 0)));
+        Assert.True(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 2, 0)));
+        Assert.True(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 3, 0)));
+        Assert.False(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 3, 1)));
+        Assert.True(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 4, 0)));
+        Assert.False(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 5, 0)));
+        Assert.False(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 6, 0)));
+        Assert.False(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 7, 0)));
+        Assert.False(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 9, 0)));
+        Assert.True(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 10, 0)));
+        Assert.True(rateLimiter.Check(token, new DateTime(2023, 4, 11, 0, 0, 11, 0)));
+    }
+
+    [Test]
+    public void TwoTokens()
+    {
+        string usToken = $"US-{Guid.NewGuid()}";
+        string euToken = $"EU-{Guid.NewGuid()}";
+        var rateLimiter = new RateLimiter(t =>
+        {
+            if (t.StartsWith("US"))
+            {
+                return new RateLimiterQueue()
+                    .EnableRateLimit(5, new TimeSpan(0, 0, 10));
+            }
+            if (t.StartsWith("EU"))
+            {
+                return new RateLimiterQueue()
+                    .EnablePeriodLimit(new TimeSpan(0, 0, 1));
+            }
+            throw new NotImplementedException();
+        });
+        Assert.True(rateLimiter.Check(usToken, new DateTime(2023, 4, 11, 0, 0, 0, 0)));
+        Assert.True(rateLimiter.Check(euToken, new DateTime(2023, 4, 11, 0, 0, 0, 1)));
+        Assert.True(rateLimiter.Check(usToken, new DateTime(2023, 4, 11, 0, 0, 0, 2)));
+        Assert.True(rateLimiter.Check(usToken, new DateTime(2023, 4, 11, 0, 0, 0, 3)));
+        Assert.False(rateLimiter.Check(euToken, new DateTime(2023, 4, 11, 0, 0, 0, 4)));
+        Assert.True(rateLimiter.Check(usToken, new DateTime(2023, 4, 11, 0, 0, 0, 5)));
+        Assert.True(rateLimiter.Check(usToken, new DateTime(2023, 4, 11, 0, 0, 0, 6)));
+        Assert.False(rateLimiter.Check(usToken, new DateTime(2023, 4, 11, 0, 0, 0, 7)));
+        Assert.True(rateLimiter.Check(euToken, new DateTime(2023, 4, 11, 0, 0, 1, 1)));
+        Assert.True(rateLimiter.Check(usToken, new DateTime(2023, 4, 11, 0, 0, 10, 0)));
+    }
 }
---
+++ RateLimiter/RateLimiter.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RateLimiter
+{
+    public class RateLimiterQueue
+    {
+        private int rate = 1;
+        private TimeSpan? duration = null;
+        private TimeSpan? period = null;
+
+        private DateTime[] timestamps = new DateTime[1];
+        private int lastTimestamp = 0;
+
+        public RateLimiterQueue()
+        {
+        }
+
+        public RateLimiterQueue EnableRateLimit(int rate, TimeSpan duration)
+        {
+            if (rate <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(rate));
+            }
+            this.timestamps = new DateTime[rate];
+            this.rate = rate;
+            this.duration = duration;
+            return this;
+        }
+
+        public RateLimiterQueue DisableRateLimit()
+        {
+            this.duration = null;
+            return this;
+        }
+
+        public RateLimiterQueue EnablePeriodLimit(TimeSpan period)
+        {
+            this.period = period;
+            return this;
+        }
+
+        public RateLimiterQueue DisablePeriodLimit()
+        {
+            this.period = null;
+            return this;
+        }
+
+        public bool Check(DateTime timestamp)
+        {
+            int index = (this.lastTimestamp + 1) % this.rate;
+            if (this.period.HasValue)
+            {
+                if (timestamp - this.timestamps[this.lastTimestamp] < this.period.Value)
+                {
+                    return false;
+                }
+            }
+            if (this.duration.HasValue)
+            {
+                if (timestamp - this.timestamps[index] < this.duration.Value)
+                {
+                    return false;
+                }
+            }
+            this.timestamps[index] = timestamp;
+            this.lastTimestamp = index;
+            return true;
+        }
+    }
+
+    public class RateLimiter
+    {
+        private Func<string, RateLimiterQueue> buildQueue;
+
+        private Dictionary<string, RateLimiterQueue> rateLimits = new();
+
+        public RateLimiter(Func<string, RateLimiterQueue> buildQueue)
+        {
+            this.buildQueue = buildQueue;
+        }
+
+        public bool Check(string token, DateTime timestamp)
+        {
+            if (!this.rateLimits.ContainsKey(token))
+            {
+                this.rateLimits.Add(token, this.buildQueue(token));
+            }
+            return this.rateLimits[token].Check(timestamp);
+        }
+    }
+}
