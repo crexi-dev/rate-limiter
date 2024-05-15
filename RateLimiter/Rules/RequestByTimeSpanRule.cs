3fc4a4d348ca0866b2c2476655cb13cfe5ff0d24
pavel.shkradziuk <ALEKSON-SH@tut.by>
2024-05-15 16:11:34 +0300

    First implementation RequestByTimeSpanRule

---
+++ RateLimiter/Rules/RequestByTimeSpanRule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RateLimiter.Rules
+{
+    public class RequestByTimeSpanRule
+    {
+        private int _requestLimit;
+
+        public RequestByTimeSpanRule(int requestLimit)
+        {
+            _requestLimit = requestLimit;
+        }
+
+        public bool Validate(int requestNumber)
+        {
+            return _requestLimit > requestNumber;
+        }
+    }
+}
