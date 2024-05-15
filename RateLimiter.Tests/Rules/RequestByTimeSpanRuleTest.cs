85e46be7a9e6a9511c47c736d1b009f200b472b5
pavel.shkradziuk <ALEKSON-SH@tut.by>
2024-05-15 16:13:33 +0300

    Create Rules folder in tests and move rule in it

---
+++ RateLimiter.Tests/Rules/RequestByTimeSpanRuleTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using RateLimiter.Rules;
+
+namespace RateLimiter.Tests.Rules;
+
+[TestFixture]
+public class RequestByTimeSpanRuleTest
+{
+    [TestCase(100, 120, ExpectedResult = false)]
+    [TestCase(120, 100, ExpectedResult = true)]
+    public bool Validate_RequestNumberMoreThanConfigured_False(int requestLimit, int requestNumber)
+    {
+        // Arrange
+        var rule = new RequestByTimeSpanRule(requestLimit);
+
+        // Act
+        var result = rule.Validate(requestNumber);
+
+        // Assert
+        return result;
+    }
+}
