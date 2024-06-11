d89dbe60dca2e61875b347533f42cd8fa5040faa
Siarhei Huliakevich <gulyakevichsv@gmail.com>
2024-06-11 20:14:05 +0300

    implement SingleRequestPerWindowLimiterRule

--- RateLimiter.Tests/RateLimiterTest.cs
+++ RateLimiter.Tests/RateLimiterTest.cs
@@ -37,6 +37,21 @@ public class RateLimiterTest
 
         rateLimiter.IsRequestAllowed("api", "user2").Should().BeTrue();
         rateLimiter.IsRequestAllowed("api", "user2").Should().BeFalse();
+    }
+
+    [Test]
+    public void SingleRequestPerWindowLimiterRule_should_allow_only_one_request_per_timespan()
+    {
+        var fakeTime = new FakeTimeProvider(startDateTime: DateTimeOffset.UtcNow);
+        var rateLimiter = new RateLimiter();
+        rateLimiter.AddRule("api", new SingleRequestPerWindowLimiterRule(fakeTime).Configure(TimeSpan.FromSeconds(5)));
+
+        rateLimiter.IsRequestAllowed("api", "user1").Should().BeTrue();
+        rateLimiter.IsRequestAllowed("api", "user1").Should().BeFalse();
+
+        rateLimiter.IsRequestAllowed("api", "user2").Should().BeTrue();
+        rateLimiter.IsRequestAllowed("api", "user2").Should().BeFalse();
+    }
 
     }
 }
---
+++ RateLimiter/Rules/SingleRequestPerWindowLimiterRule.cs
@@ -0,0 +1,38 @@
+﻿using RateLimiter.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace RateLimiter.Rules;
+public class SingleRequestPerWindowLimiterRule(TimeProvider timeProvider): IRule
+{
+    internal TimeSpan? _requestTimeSpanLimit;
+
+    private readonly Dictionary<string, DateTimeOffset> usersLastCall = [];
+
+    public SingleRequestPerWindowLimiterRule Configure(TimeSpan requestTimeSpanLimit)
+    {
+        _requestTimeSpanLimit = requestTimeSpanLimit;
+        return this;
+    }
+
+    public bool IsRequestAllowed(string token)
+    {
+        if (_requestTimeSpanLimit is null)
+        {
+            throw new ArgumentException($"Call {nameof(Configure)} method to configure this rule first");
+        }
+
+        var currentTime = timeProvider.GetUtcNow();
+
+        if (usersLastCall.TryGetValue(token, out var lastCall) && lastCall + _requestTimeSpanLimit >= currentTime)
+        {
+            return false;
+        }
+        else
+        {
+            usersLastCall[token] = currentTime;
+        }
+
+        return true;
+    }
+}
