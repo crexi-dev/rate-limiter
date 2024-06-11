75b24ed2657d42936c1b06c4a9ca44f67e99cf43
Siarhei Huliakevich <gulyakevichsv@gmail.com>
2024-06-11 19:36:59 +0300

    implement FixedWindow rate limit rule

--- RateLimiter.Tests/RateLimiterTest.cs
+++ RateLimiter.Tests/RateLimiterTest.cs
@@ -1,4 +1,8 @@
-﻿using NUnit.Framework;
+﻿using FluentAssertions;
+using Microsoft.Extensions.Time.Testing;
+using NUnit.Framework;
+using RateLimiter.Rules;
+using System;
 
 namespace RateLimiter.Tests;
 
@@ -6,8 +10,33 @@ namespace RateLimiter.Tests;
 public class RateLimiterTest
 {
 	[Test]
-	public void Example()
+	public void FixedWindowLimiterRule_should_not_allow_excessive_requests()
 	{
-		Assert.That(true, Is.True);
-	}
+        var fakeTime = new FakeTimeProvider(startDateTime: DateTimeOffset.UtcNow);
+        var rateLimiter = new RateLimiter();
+		rateLimiter.AddRule("api", new FixedWindowLimiterRule(fakeTime).Configure(new FixedWindowLimiterRuleConfiguration(Window: TimeSpan.FromSeconds(5), Limit: 2)));
+
+		rateLimiter.IsRequestAllowed("api", "user1").Should().BeTrue();
+        rateLimiter.IsRequestAllowed("api", "user1").Should().BeTrue();
+        rateLimiter.IsRequestAllowed("api", "user1").Should().BeFalse();
+
+        fakeTime.Advance(TimeSpan.FromSeconds(6));
+
+        rateLimiter.IsRequestAllowed("api", "user1").Should().BeTrue();
+    }
+
+    [Test]
+    public void FixedWindowLimiterRule_should_have_separate_limits_for_different_users()
+    {
+        var fakeTime = new FakeTimeProvider(startDateTime: DateTimeOffset.UtcNow);
+        var rateLimiter = new RateLimiter();
+        rateLimiter.AddRule("api", new FixedWindowLimiterRule(fakeTime).Configure(new FixedWindowLimiterRuleConfiguration(Window: TimeSpan.FromSeconds(5), Limit: 1)));
+
+        rateLimiter.IsRequestAllowed("api", "user1").Should().BeTrue();
+        rateLimiter.IsRequestAllowed("api", "user1").Should().BeFalse();
+
+        rateLimiter.IsRequestAllowed("api", "user2").Should().BeTrue();
+        rateLimiter.IsRequestAllowed("api", "user2").Should().BeFalse();
+
+    }
 }
---
+++ RateLimiter/Rules/FixedWindowLimiterRule.cs
@@ -0,0 +1,56 @@
+﻿using RateLimiter.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace RateLimiter.Rules;
+public class FixedWindowLimiterRule(TimeProvider timeProvider) : IRule
+{
+    private FixedWindowLimiterRuleConfiguration? _configuration;
+
+    private readonly Dictionary<string, FixedWindow> windowPerUser = [];
+
+    public FixedWindowLimiterRule Configure(FixedWindowLimiterRuleConfiguration configuratuion)
+    {
+        _configuration = configuratuion;
+        return this;
+    }
+
+    public bool IsRequestAllowed(string token)
+    {
+        if (_configuration is null)
+        {
+            throw new ArgumentException($"Call {nameof(Configure)} method to configure this rule first");
+        } 
+
+        var currentTime = timeProvider.GetUtcNow();
+
+        var currentWindow = windowPerUser.GetValueOrDefault(token);
+
+        // If there is a new user OR it is time to start a new window,
+        // initialize a new fixed window with the current request time.
+        if (currentWindow is null
+        || currentWindow.Start + _configuration.Window < currentTime)
+        {
+            currentWindow = new FixedWindow(currentTime, 0);
+        }
+
+
+        // If a number of requests within the window exceeds the limit,
+        // disallow this request. Otherwise, update the current request count
+        // and allow the request.
+        if (currentWindow.Count >= _configuration.Limit)
+        {
+            return false;
+        }
+        else
+        {
+            var updatedWindow = currentWindow with { Count = currentWindow.Count + 1 };
+            windowPerUser[token] = updatedWindow;
+            return true;
+        }
+    }
+}
+
+public record FixedWindowLimiterRuleConfiguration(TimeSpan Window, int Limit);
+
+internal record FixedWindow(DateTimeOffset Start, int Count);
